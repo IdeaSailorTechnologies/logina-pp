e243820b975c214a51515bc1ce0c8aee5e83aae6
Gaikwad <Abhishek.Gaikwad@FISGLOBAL.COM>
2019-05-29 19:42:37 +0530

    Removed material design

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,14 +1,12 @@
-import {NgModule}  from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {UserComponent} from '../user/user.component';
-import {LoginComponent} from '../login/login.component';
-import {RegistrationComponent } from '../registration/registration.component';
-import { from } from 'rxjs';
+import { UserComponent } from '../user/user.component';
+import { LoginComponent } from '../login/login.component';
+
 const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'login', component: LoginComponent },
-  {path : '', component : LoginComponent},
-  {path : 'registration', component: RegistrationComponent }
+  { path: '', component: LoginComponent }
 ];
 @NgModule({
   imports: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {MatDialog} from '@angular/material'
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,16 +8,16 @@ import {MatDialog} from '@angular/material'
 })
 export class LoginComponent implements OnInit {
   constructor(private router: Router) { }
-username: string;
-password: string;
+  username: string;
+  password: string;
   ngOnInit() {
   }
-  login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["user"]);
-    }else {
+  login(): void {
+    if (this.username == 'admin' && this.password == 'admin') {
+      this.router.navigate(["user"]);
+    } else {
       alert("Invalid credentials");
     }
   }
-  }
+}
 
