8cacc05c493c246393d397dee99d9641fa476479
IdeaSailorTechnologies <you@example.com>
2019-06-02 11:56:11 +0530

    adding registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { AppRoutingModule } from './core/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
 import { NbThemeModule, NbButtonModule, NbInputModule, NbCardModule, NbLayoutModule } from '@nebular/theme';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
