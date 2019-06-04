3eed20759bf9e4ef15b88cc3154ddcc3c025d127
IdeaSailorTechnologies <you@example.com>
2019-06-04 18:38:57 +0530

    Add sidebar componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,33 @@ import { AppRoutingModule } from './core/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NbThemeModule, NbButtonModule, NbInputModule, NbCardModule, NbLayoutModule } from '@nebular/theme';
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+import { NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './registration/registration.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     FormsModule,
-    NbInputModule,
-    NbCardModule,
-    NbButtonModule,
+    NbThemeModule.forRoot(),
+    RouterModule,
+    NbSidebarModule,
+    NbLayoutModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'corporate' }),
     NbLayoutModule
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { RegistrationComponent } from '../registration/registration.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 
 const routes: Routes = [
-  
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent },
-  { path: 'registration', component: RegistrationComponent }
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'sidebar', component: SidebarComponent}
 ];
 @NgModule({
   imports: [
