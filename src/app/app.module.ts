fb935ebc2b071bc517a62cd7ebdc34d0d4f07635
ideasailortechnologies@gmail.com <ideasailortechnologies@gmail.com>
2019-06-13 16:46:09 +0530

    removed validation part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ForgotpassComponent } from './forgotpass/forgotpass.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+   
     AppRoutingModule,
     FormsModule,
     NbThemeModule.forRoot(),
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
@@ -8,28 +9,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router,private formBuilder: FormBuilder) { }
+  constructor(private router: Router ) { }
   username: any;
   password: any;
-  registerForm: FormGroup;
-    submitted = false;
+ //registerForm: FormGroup;
+ //   submitted = false;
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-      
-  });
+
   }
-  get f() { return this.registerForm.controls; }
+  
   login(): void {
-    this.submitted = true;
-    if (this.registerForm.invalid) {
+   
     if (this.username === 'admin' && this.password === 'admin') {
       this.router.navigate(['sidebar']);
     } else {
       alert('Invalid credentials');
-    }
+    
   }
   }
 }
